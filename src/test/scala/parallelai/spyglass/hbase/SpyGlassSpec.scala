8e8ea629c0c0ae2d2bba27b2dc587678c25e0d1f
Koert Kuipers <koert@tresata.com>
2013-08-05 18:17:42 -0400

    ugly check for results in tap

--- src/test/scala/parallelai/spyglass/hbase/SpyGlassSpec.scala
+++ src/test/scala/parallelai/spyglass/hbase/SpyGlassSpec.scala
@@ -4,10 +4,11 @@ import java.io.File
 import scala.collection.JavaConverters._
 import com.google.common.io.Files
 import cascading.pipe.Pipe
+import cascading.tuple.{ TupleEntry, Fields }
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.util.ToolRunner
 import org.apache.hadoop.hbase.util.Bytes
-import com.twitter.scalding.{ Tool, Job, Args, Csv }
+import com.twitter.scalding.{ Tool, Job, Args, Csv, Hdfs, Read }
 import org.apache.hadoop.hbase.zookeeper.MiniZooKeeperCluster
 import org.apache.hadoop.hbase.{ HBaseConfiguration, MiniHBaseCluster, HTableDescriptor, HColumnDescriptor, TableExistsException }
 import org.apache.hadoop.hbase.client.{ HBaseAdmin, HTable, Scan, Delete, Result }
@@ -48,27 +49,42 @@ object SpyGlassSpec {
 
   def toMaps(results: Array[Result], family: String): Map[String, Map[String, String]] =
     results.map{ result => (Bytes.toString(result.getRow), toMap(result, family)) }.toMap
+
+  def toMap(te: TupleEntry): Map[String, String] = (te.getFields.iterator.asScala zip te.getTuple.iterator.asScala)
+    .filter{ case (field, item) => item != null }
+    .map{ case (field, item) => (field.toString, item.toString) }
+    .toMap
 }
 
 class SpyGlassSpec extends FunSpec with BeforeAndAfterAll {
   import SpyGlassSpec._
 
   val conf = new HBaseConfiguration
+  val mode = Hdfs(true, conf)
   val tmpDir = Files.createTempDir
   val zkCluster = new MiniZooKeeperCluster
   conf.set("hbase.zookeeper.property.clientPort", zkCluster.startup(tmpDir).toString)
   val hbaseCluster = new MiniHBaseCluster(conf, 1)
   val table = createTable(conf, "test", "cf")
 
+  val checkSet = Set(
+    Map("first" -> "joe", "last" -> "biden", "party" -> "democrats", "rating" -> "5"),
+    Map("first" -> "paul", "last" -> "ryan", "rating" -> "3")
+  )
+  val checkMap = checkSet.map(x => (x("first"), x - "first")).toMap
+
   describe("An HBaseSource") {
     it("should be able to participate in a flow as a sink and source") {
-      val checkMap = Map(
-        "joe" -> Map("last" -> "biden", "party" -> "democrats", "rating" -> "5"),
-        "paul" -> Map("last" -> "ryan", "rating" -> "3"))
       truncTable(table)
       ToolRunner.run(conf, new Tool, Array("parallelai.spyglass.hbase.LoadJob", "--hdfs"))
       ToolRunner.run(conf, new Tool, Array("parallelai.spyglass.hbase.ExtractJob", "--hdfs"))
       assert(toMaps(table.getScanner(Bytes.toBytes("cf")).next(1000), "cf") == checkMap)
+      assert(
+        mode.openForRead(
+          Csv("file://" + SpyGlassSpec.getLocalPath("tmp/candidates").toString, fields = new Fields("first", "last", "party", "rating"), separator = "|", quote = null)
+            .createTap(Read)(mode)
+        ).asScala.forall{ te => checkSet.contains(toMap(te)) }
+      )
     }
   }
 
