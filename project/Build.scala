1b52540b4c7cd918a17df3cd7531a2d70d2c71ac
Koert Kuipers <koert@tresata.com>
2013-11-25 12:47:39 -0500

    bump versions and dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -8,19 +8,19 @@ object SpyglassBuild extends Build {
     settings = Project.defaultSettings ++ Seq(
       organization := "parallelai",
       name := "spyglass",
-      version := "0.1-tres",
+      version := "0.2-tres-SNAPSHOT",
       scalaVersion := "2.9.3",
       retrieveManaged := true,
       retrievePattern := "[artifact](-[revision])(-[classifier]).[ext]",
       libraryDependencies ++= Seq(
-        "com.twitter" %% "scalding-core" % "0.9.0-tres-rc1" % "compile",
+        "com.twitter" %% "scalding-core" % "0.9.0rc5-tres-SNAPSHOT" % "compile",
         "org.apache.hadoop" % "hadoop-core" % "1.0.4" % "provided",
         "org.apache.hbase" % "hbase" % "0.94.6" % "provided",
         "org.slf4j" % "slf4j-log4j12" % "1.6.6" % "provided",
         "com.novocode" % "junit-interface" % "0.8" % "test",
         "org.scalatest" %% "scalatest" % "1.9.2" % "test",
         "org.apache.hbase" % "hbase" % "0.94.6" % "test" classifier "tests",
-        "com.google.guava" % "guava" % "13.0" % "test"
+        "com.google.guava" % "guava" % "14.0.1" % "test"
       ),
       resolvers ++= Seq( // necessary since scalding is not pusblished yet
         "releases" at "http://server01:8080/archiva/repository/internal",
