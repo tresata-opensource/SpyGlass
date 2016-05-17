726c985eb7a38bc9c9b686e46cea227f39fd7125
Koert Kuipers <koert@tresata.com>
2016-05-16 22:58:59 -0400

    roll to next snapshot

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object SpyglassBuild extends Build {
     Seq(
       organization := "parallelai",
       name := "spyglass",
-      version := "0.8.0-tres",
+      version := "0.9.0-tres-SNAPSHOT",
       scalaVersion := "2.11.7",
       retrieveManaged := true,
       retrievePattern := "[artifact](-[revision])(-[classifier]).[ext]",
